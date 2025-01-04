c37712087bd8b7ecdeb111153d1b3533a496c470
ashil rahim <ashilrahim2@gmail.com>
2025-01-04 21:22:56 +0530

    added a review routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,10 +6,12 @@ import dotenv from "dotenv";
 // import userRoutes from "./routes/userRoutes.js";
 import eventOwnerRoutes from "./routes/eventOwnerRoutes.js"
 import bookingRoutes from "./routes/bookingRoutes.js"
+import reviewRoutes from "./routes/reviewRoutes.js"
 import authRoutes from "./routes/authRoutes.js"
 import adminroutes from "./routes/adminRoutes.js"
 // import { errorHandler } from "./middleware/errorHandler.js";
 import { connectDB } from "./config/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -20,10 +22,11 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser())
 
 // Routes
 app.use('/api/auth', authRoutes);
-// app.use("/api/users", userRoutes);
+app.use('/api/reviews', reviewRoutes);
 app.use('/api/event-owners', eventOwnerRoutes);
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/admin', adminroutes);
