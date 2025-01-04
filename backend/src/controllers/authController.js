c89f652fb166b1971fd8755d3567bc95f095699b
ashil rahim <ashilrahim2@gmail.com>
2025-01-04 21:24:20 +0530

    refactor: update authController to use utility functions for token generation and cookie handling

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,7 +1,8 @@
 // controllers/userController.js
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import { generateToken, sendTokenInCookie } from "../config/utils.js";
 
 export const register = async (req, res) => {
   try {
@@ -9,7 +10,7 @@ export const register = async (req, res) => {
 
     const userExists = await User.findOne({ email });
     if (userExists) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ message: "User already exists" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -18,24 +19,40 @@ export const register = async (req, res) => {
       email,
       password: hashedPassword,
       contactNumber,
-      role
+      role: role || "user",
     });
 
-    const token = jwt.sign(
-      { userId: user._id, role: user.role },
-      process.env.JWT_SECRET,
-      { expiresIn: '24h' }
-    );
+    const token = generateToken(user._id, user.role);
 
-    res.status(201).json({
-      token,
-      user: {
-        id: user._id,
-        username: user.username,
-        email: user.email,
-        role: user.role
-      }
-    });
+    // for cookies
+    sendTokenInCookie(res, token);
+
+
+    // If the role is 'event_owner', send a response to move to event owner profile creation
+    if (role === "event_owner") {
+      res.status(201).json({
+        message:
+          "Account created successfully as an event owner. Please proceed to complete your business profile.",
+        user: {
+          id: user._id,
+          username: user.username,
+          email: user.email,
+          role: user.role,
+        },
+      });
+    } else {
+      
+      res.status(201).json({
+
+        message: "Account created successfully",
+        user: {
+          id: user._id,
+          username: user.username,
+          email: user.email,
+          role: user.role,
+        },
+      });
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -47,28 +64,27 @@ export const login = async (req, res) => {
     const user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
-      return res.status(400).json({ message: 'Invalid password' });
+      return res.status(400).json({ message: "Invalid password" });
     }
 
-    const token = jwt.sign(
-      { userId: user._id, role: user.role },
-      process.env.JWT_SECRET,
-      { expiresIn: '24h' }
-    );
+    const token = generateToken(user._id, user.role);
 
-    res.json({
-      token,
+    // for cookies
+    sendTokenInCookie(res, token);
+
+    res.status(201).json({
+      message: "Login successful",
       user: {
         id: user._id,
         username: user.username,
         email: user.email,
-        role: user.role
-      }
+        role: user.role,
+      },
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -76,20 +92,20 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-    try {
-      res.cookie("jwt", "", { maxAge: 0 });
-      res.status(200).json({ message: "Logged out Successfully" });
-    } catch (error) {
-      console.log("Error in Logout Controller", error.message);
-      res.status(500).json({ message: "Internal Server Error " });
-    }
-  };
+  try {
+    res.clearCookie("token");
+    res.status(200).json({ message: "Logged out Successfully" });
+  } catch (error) {
+    console.log("Error in Logout Controller", error.message);
+    res.status(500).json({ message: "Internal Server Error " });
+  }
+};
 
 export const checkAuth = (req, res) => {
-    try {
-      res.status(200).json(req.user);
-    } catch (error) {
-      console.log("Error in checkAuth controller", error.message);
-      res.status(500).json({ message: "Internal Server Error" });
-    }
-  };
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
