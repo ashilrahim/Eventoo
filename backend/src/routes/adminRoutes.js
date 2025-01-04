71904d351d82ddb04e618391a400c032cfce1f82
ashil rahim <ashilrahim2@gmail.com>
2025-01-04 21:25:41 +0530

    feat: add review management routes for admin including CRUD operations

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -2,13 +2,13 @@ import express from 'express';
 import { 
   getAllUsers, 
   getAllEventOwners, 
-  getAllBookings, 
   updateEventOwnerStatus, 
   deleteUser, 
   deleteEventOwner, 
-  deleteBooking 
 } from "../controllers/adminController.js";
 import { authenticateToken, authorizeRole } from '../middleware/auth.js'; // Import middlewares
+import { deleteReview, getAllReviews, getReview } from '../controllers/reviewController.js';
+import { deleteBooking, getAllBookings, getBookingById } from '../controllers/bookingController.js';
 
 const router = express.Router();
 
@@ -16,9 +16,12 @@ const router = express.Router();
 router.get('/users', authenticateToken, authorizeRole('admin'), getAllUsers);
 router.get('/event-owners', authenticateToken, authorizeRole('admin'), getAllEventOwners);
 router.get('/bookings', authenticateToken, authorizeRole('admin'), getAllBookings);
-
+router.get('/bookings/:id', authenticateToken, authorizeRole('admin'), getBookingById);
+router.delete('/bookings/:id', authenticateToken, authorizeRole('admin'), deleteBooking);
+router.get('/review', authenticateToken, authorizeRole('admin'), getAllReviews);
+router.get('/review/:id', authenticateToken, authorizeRole('admin'), getReview); 
+router.delete('/review/:id', authenticateToken, authorizeRole('admin'), deleteReview);
 router.put('/event-owners/:id/status', authenticateToken, authorizeRole('admin'), updateEventOwnerStatus);
-
 router.delete('/users/:id', authenticateToken, authorizeRole('admin'), deleteUser);
 router.delete('/event-owners/:id', authenticateToken, authorizeRole('admin'), deleteEventOwner);
 router.delete('/bookings/:id', authenticateToken, authorizeRole('admin'), deleteBooking);
