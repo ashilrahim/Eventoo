0c8a8c2d0c8e546a1a711c9a886b0bc3480a2716
ashil rahim <ashilrahim2@gmail.com>
2025-01-09 11:31:57 +0530

    feat: update booking model to support multiple event dates and add days field

--- backend/src/models/Booking.js
+++ backend/src/models/Booking.js
@@ -1,14 +1,23 @@
 // models/Booking.js
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  eventOwnerId: { type: mongoose.Schema.Types.ObjectId, ref: 'EventOwner', required: true },
-  eventDate: { type: Date, required: true },
-  status: { type: String, enum: ['pending', 'confirmed', 'completed', 'cancelled'], default: 'pending' },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  eventOwnerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "EventOwner",
+    required: true,
+  },
+  eventDates: [{ type: Date, required: true }], // Changed to array of dates
+  Days: { type: Number, required: true },
+  status: {
+    type: String,
+    enum: ["pending", "confirmed", "completed", "cancelled"],
+    default: "pending",
+  },
   totalAmount: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
-  paymentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Payment' }  // Reference to the payment
+  paymentId: { type: mongoose.Schema.Types.ObjectId, ref: "Payment" },
 });
 
-export default mongoose.model('Booking', bookingSchema);
+export default mongoose.model("Booking", bookingSchema);
